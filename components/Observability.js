b3022380970f3d4e1fb65ce462c505572fad9966
Winston Hearn <winstonhearn@honeycomb.io>
2022-04-27 16:08:44 -0400

    clean up and lots of comments

--- components/Observability.js
+++ components/Observability.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import {getCLS, getFID, getLCP, getTTFB} from 'web-vitals';
 
+// Threshold to weed out insignificant Layout Shift events
 const CLS_THRESHOLD = .02;
+// Simple generic session ID that will help us query all events on a given session
 const sessionID =  '_' + Math.random().toString(36).substr(2, 9);
 
 let metadata = {};
 
+// This method is called on initial load and lets us capture all metadata
+// about the browser and device that might help us dig into patterns
 function captureMetadata() {
   metadata = {
     // Pixel dimensions of the visible screen
@@ -21,12 +25,14 @@ function captureMetadata() {
   }
 }
 
+// Loops through all CLS events (there can be dozens) to filter out minor ones
+// and then pulls out helpful debugging info for all shifts to pass to Honeycomb
 function extractLargeShifts(entries) {
   let shifts = {};
   let i = 0;
 
   entries.forEach((shift) => {
-    // weed out super minor shifts
+    // Adjust the CLS Threshold for to include more events
     if (shift.value >= CLS_THRESHOLD) {
       
       const resp = shifts[`shift_${i+=1}`] = {
@@ -36,11 +42,16 @@ function extractLargeShifts(entries) {
       let classLists = [];
       let parents = [];
 
+      // 'source' is the CWV identified culprit for a layout shift
       shift.sources.forEach((source) => { 
+        // This grabs all classes on the source element, to help identify the where on page it is
         classLists.concat([...source.node.classList]);
+        // This grabs the classes on the source element's parent element
         parents.concat([...source.node.parentElement.classList]);
+        // Initial height and width of the element that triggered a layout shift
         resp.initialHeight = source.previousRect.height;
         resp.initialWidth = source.previousRect.width;
+        // End height and width of the element. This gives us the pixel value of layout shift size.
         resp.endHeight = source.currentRect.height;
         resp.endWidth = source.currentRect.width;
       });
@@ -53,7 +64,8 @@ function extractLargeShifts(entries) {
   return shifts;
 }
 
-async function handleCLSEvent(evt) {
+// handler for Cumulative Layout Shift
+function handleCLSEvent(evt) {
   let report = {
     name: evt.name,
     delta: evt.delta,
@@ -62,23 +74,11 @@ async function handleCLSEvent(evt) {
     ...metadata
   };
 
-  console.log(report);
-  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric: report })
+  send(report);
 }
 
-async function reportFID(metric) {
-  const report = {
-    name: metric.name,
-    value: metric.value,
-    delta: metric.delta,
-    ...metadata
-  }
-
-  console.log(report);
-  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric: report })
-}
-
-async function reportLCP(metric) {
+// Handler for Largest Contentful Paint
+function reportLCP(metric) {
   const report = {
     name: metric.name,
     value: metric.value,
@@ -87,36 +87,42 @@ async function reportLCP(metric) {
   };
 
   if (metric.entries.length > 0 ) {
+    // It's possible we need to loop through all entries
     let lcp = metric.entries[0];
+    // Computed pixel size of the largest content
     report.size = lcp.size;
+    // Time it took (from page start load) to load the content
     report.duration = lcp.duration;
+    // url if the largest content is media
     report.url = lcp.url;
-
   }
 
-  console.log(report);
-  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric: report })
+  send(report);
 }
 
-async function reportTTFB(metric) {
+// Handler for Time to First Bite and First Input Delay
+function reportTiming(metric) {
   const report = {
     name: metric.name,
     value: metric.value,
     delta: metric.delta,
     ...metadata
   }
+  send(report);
+}
 
-  console.log(report);
-  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric: report })
+async function send(metric) {
+  console.log(metric);
+  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric })
 }
 
 
 export default function ({children}) {
   captureMetadata();
   getCLS(handleCLSEvent);
-  getFID(reportFID);
+  getFID(reportTiming);
   getLCP(reportLCP);
-  getTTFB(reportTTFB);
+  getTTFB(reportTiming);
   return (
     <>
       {children}
