ecc4f5611c029e303eb84d048b738d254e77e737
Winston Hearn <winstonhearn@honeycomb.io>
2022-05-20 12:11:45 -0400

    huge refactor

--- components/Observability.js
+++ components/Observability.js
@@ -1,24 +1,29 @@
-import axios from 'axios';
-import {getCLS, getFID, getLCP} from 'web-vitals';
+import {getCLS, getFID, getLCP, getTTFB} from 'web-vitals';
 
 // Threshold to weed out insignificant Layout Shift events
 const CLS_THRESHOLD = .02;
 // Simple generic session ID that will help us query all events on a given session
 const sessionID =  '_' + Math.random().toString(36).substr(2, 9);
-
+// make it easy to generate a unique id
+function generateId() {
+  return Math.random().toString(36).substr(2, 20);
+};
+// this will be populated on init
 let metadata = {};
+let lastSpanID = null;
+let trace_id = null;
 
 // This method is called on initial load and lets us capture all metadata
 // about the browser and device that might help us dig into patterns
 function captureMetadata() {
   metadata = {
+    name: 'web_performance',
     pathname: document.location.pathname, 
     // Pixel dimensions of the visible screen
     screenWidth: window.innerWidth,
     screenHeight: window.innerHeight,
     // Browser string
-    browser: navigator.userAgent,
-    sessionID
+    browser: navigator.userAgent
   }
 
   if (navigator.userAgentData) {
@@ -27,6 +32,24 @@ function captureMetadata() {
     // Browser vendor
     metadata.vendor = navigator.userAgentData.vendor || null;
   }
+}
+
+function createRootSpan() {
+  trace_id = sessionStorage.getItem('HNY_TRACE');
+  captureMetadata();
+  // trace id is generated once and used for all session pageviews
+  if (trace_id) return true;
+
+  // if trace_id does not exist in session storage, this is the first pageview of the session
+  trace_id = generateId();
+  lastSpanID = generateId();
+  sessionStorage.setItem('HNY_TRACE', trace_id);
+
+  send({
+    span_event: 'root',
+    trace_id,
+    ...metadata
+  });
 }
 
 // -----------------------------------
@@ -69,18 +92,29 @@ function getPerformanceMeasures() {
   return output;
 }
 
+// Handler for TTFB
+function reportInitialTiming(metric) {
+  send({
+    span_event: metric.name,
+    ttfb_value: metric.value,
+    ttfb_delta: metric.delta,
+    ...metadata,
+    trace_id
+  });
+}
+
 // Handler for First Input Delay
 function reportScriptTiming(metric) {
-  const report = {
-    name: metric.name,
+  send({
+    span_event: metric.name,
     fid_value: metric.value,
     fid_delta: metric.delta,
-    scriptsOnPage: document.scripts.length,
+    trace_id,
+    scriptsOnPage: document.scripts.length + 1,
     scripts: captureScriptData(),
     ...getPerformanceMeasures(),
     ...metadata
-  }
-  send(report);
+  });
 }
 
 // ----------------------------------------------
@@ -96,15 +130,14 @@ function extractLargeShifts(entries) {
     // Adjust the CLS Threshold for to include more events
     if (shift.value >= CLS_THRESHOLD) {
       let evt = {
-        value: shift.value,
-        hadRecentInput: shift.hadRecentInput,
-        sources: []
+        cls_shift_value: shift.value,
+        hadRecentInput: shift.hadRecentInput
       }
         
       // 'source' is the CWV identified culprit for a layout shift
       shift.sources.forEach((source, i) => { 
         const el = source.node;
-        evt.sources.push({
+        evt[`shift_${i}`] = {
           // This grabs all classes on the source element, to help identify the where on page it is
           sourceEl: el ? `${el.localName}.${[...el.classList].join('.')}` : null,
           // This grabs the classes on the source element's parent element
@@ -112,14 +145,13 @@ function extractLargeShifts(entries) {
           // This grabs the classes on the source element's previous sibling
           previousSiblingEl: el ? `${el.previousSibling.localName}.${[...el.previousSibling.classList].join('.')}`: null,
           // Initial height and width of the element that triggered a layout shift
-          initialHeight: source.previousRect.height,
-          initialWidth: source.previousRect.width,
+          startHeight: source.previousRect.height,
+          startWidth: source.previousRect.width,
           // End height and width of the element. This gives us the pixel value of layout shift size.
           endHeight: source.currentRect.height,
           endWidth: source.currentRect.width,
-        });        
+        };        
       });
-      shifts[`shift_${i}`] = evt;
     }
   });
   return shifts;
@@ -128,9 +160,10 @@ function extractLargeShifts(entries) {
 // handler for Cumulative Layout Shift
 function handleCLSEvent(evt) {
   let report = {
-    name: evt.name,
+    span_event: evt.name,
     cls_delta: evt.delta,
     cls_value: evt.value,
+    trace_id,
     ...extractLargeShifts(evt.entries),
     ...metadata
   };
@@ -144,10 +177,12 @@ function handleCLSEvent(evt) {
 
 // Handler for Largest Contentful Paint
 function reportLCP(metric) {
+  console.log('web-vitals LCP', metric);
   const report = {
-    name: metric.name,
+    span_event: metric.name,
     lcp_value: metric.value,
     lcp_delta: metric.delta,
+    trace_id,
     ...metadata
   };
 
@@ -156,29 +191,44 @@ function reportLCP(metric) {
 
   // outputs element type and classlist as a string
   if (lcp.element) {
-    report.elementSelector = `${lcp.element.localName}.${[...lcp.element.classList].join('.')}`;  
+    report.lcp_elementSelector = `${lcp.element.localName}.${[...lcp.element.classList].join('.')}`;  
   }
   // Computed pixel size of the largest content
-  report.sizeInPixels = lcp.size;
+  report.lcp_sizeInPixels = lcp.size;
   // url if the largest content is media
-  report.url = lcp.url;
+  report.lcp_url = lcp.url;
   // time it took the browser to load the element
-  report.loadTimeMS = lcp.loadTime
+  report.lcp_loadTimeMS = lcp.loadTime
 
   send(report);
 }
 
-
+// Send events along to the api endpoint from /pages/api/index.js
 async function send(metric) {
-  console.log(metric);
-  await axios.put(`${process.env.NEXT_PUBLIC_ENDPOINT}`, { metric })
+  // generate these at the point of send
+  metric.span_id = generateId();
+  metric.timestamp = Date.now();
+
+  if (!metric.span_event === 'root') {
+    metric.parent_id = lastSpanID;
+  }
+
+  await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(metric)
+  });
+  lastSpanID = metric.span_id;
 }
 
 export default function () {
-  captureMetadata();
+  createRootSpan();
   getCLS(handleCLSEvent);
   getFID(reportScriptTiming);
   getLCP(reportLCP);
+  getTTFB(reportInitialTiming);
   return (
     <></>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import dynamic from 'next/dynamic';
 import '../styles/globals.css'
 
+export function reportWebVitals(metric) {
+  if (metric.label === 'web-vital') {
+    console.log('next.js', metric) // The metric object ({ id, name, startTime, value, label }) is logged to the console
+  }
+}
+
+
 // WebVitals reporting depends on document, so this needs to be lazy-imported to avoid error
 const WebPerformanceObserver = dynamic(
   () => import('../components/Observability'),
