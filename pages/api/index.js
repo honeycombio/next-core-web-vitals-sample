68c6c453564a77dfcd70d20361c0d3cede6007ed
Winston Hearn <winstonhearn@honeycomb.io>
2022-05-20 13:55:40 -0400

    logging

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,13 +1,12 @@
 import Libhoney from 'libhoney';
 
-// Hey winston!
-// The dataset is in dogfood's Testing environment.
-// Set the ky and dataset value from there.
 const hny = new Libhoney({
   apiHost: 'https://api-dogfood.honeycomb.io',
   writeKey: process.env.HNY_API_KEY,
   dataset: process.env.HNY_DATASET,
 });
+
+console.log(process.env.HNY_DATASET);
 
 function sendEvent(metric) {
   const evnt = hny.newEvent();
@@ -16,22 +15,20 @@ function sendEvent(metric) {
 }
 
 export default function handler(req, res) {
-    const metric = req.body;
-    const allowedList = ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'root'];
+  const metric = req.body;
+  const allowedList = ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'root'];
+  console.log(metric);
   try {
+
     if (allowedList.includes(metric.span_event)) {
       sendEvent(metric);
     }
-
     res.status(200);
-    return res.end();  
+    res.end();  
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({msg: error.message})
     }
   }
-    
-
-    
 }
